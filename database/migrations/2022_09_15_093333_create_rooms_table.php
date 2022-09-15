710f6d6655a0a9bcb35f1bb64b84239a95f42daf
Eriri-Spencer20 <radityaappj23@gmail.com>
2022-09-15 19:20:42 +0700

    create room model

---
+++ database/migrations/2022_09_15_093333_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('no_rooms');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->integer('price');
+            // $table->enum(('type_room'), ['1', '2', '3', '4'])->default('1');
+            $table->enum('status', ['available', 'not available']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
